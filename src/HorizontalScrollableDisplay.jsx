67a23b24b2cb403bf34ceac9e395f83f5a6e1a4d
Bryan Dalton <bryantdalton19@gmail.com>
2024-04-27 10:32:53 -0400

    files created

---
+++ src/HorizontalScrollableDisplay.jsx
@@ -0,0 +1,17 @@
+import "/HorizontalScrollableDisplay.css";
+
+const images = [
+    
+]
+
+export default function HorizontalScrollableDisplay() {
+  return (
+    <>
+      <div className="container">
+        <div className="scrollable"></div>
+
+        <div className="scrollable"></div>
+      </div>
+    </>
+  );
+}
